70256db7f56a9f7b5d75b17dbe6b34354418b2c2
LittleApple-fp16 <148928493+LittleApple-fp16@users.noreply.github.com>
2024-01-09 17:13:56 +0800

    fix

--- post_test.py
+++ post_test.py
@@ -4,4 +4,4 @@ import os
 hug_fs = HfFileSystem(token=os.environ.get('HF_TOKEN'))
 hugApi_fs = HfApi(token=os.environ.get('HF_TOKEN'))
 hug_fs_sec = HfFileSystem()
-print(f"Result with token: {hug_fs.exists(f'{repo_name}/hf_test/.gitattributes')} And Result with no token: {hug_fs_sec.exists(f'{repo_name}/hf_test/.gitattributes')} And Result with HfApi: {hugApi_fs.file_exists(f'{repo_name}/hf_test/.gitattributes')}")
+print(f"Result with token: {hug_fs.exists(f'{repo_name}/hf_test/.gitattributes')} And Result with no token: {hug_fs_sec.exists(f'{repo_name}/hf_test/.gitattributes')} And Result with HfApi: {hugApi_fs.file_exists(f'{repo_name}/hf_test', filename='.gitattributes', repo_type='model')}")
