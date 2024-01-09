9a12f58b756a32f0f695cf5c1eff321e93ad9080
LittleApple-fp16 <148928493+LittleApple-fp16@users.noreply.github.com>
2024-01-09 15:29:18 +0800

    fix

--- post_test.py
+++ post_test.py
@@ -1,5 +1,6 @@
 from huggingface_hub import CommitOperationAdd, CommitOperationDelete, configure_http_backend, HfApi, HfFileSystem
 from fs_test import repo_name
+import os
 hug_fs = HfFileSystem(token=os.environ.get('HF_TOKEN'))
 hug_fs_sec = HfFileSystem()
 print(f"Result with token: {hug_fs.exists(f'{repo_name}/hf_test/.gitattributes')} And Result with no token: {hug_fs_sec.exists(f'{repo_name}/hf_test/.gitattributes')}")
